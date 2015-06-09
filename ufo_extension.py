9d107bf8f64f62205a3ddfad22c8a73ba6dadaa0
Chuan Miao <chuan.miao@kit.edu>
2015-06-09 10:03:12 +0200

    update UfoGraph.merge_branchin ufo_extension

--- ufo_extension.py
+++ ufo_extension.py
@@ -6,22 +6,25 @@ class TaskGraph(Ufo.TaskGraph):
             n0 = node_list[i]
             n1 = node_list[i+1]
             self.connect_nodes(n0, n1)
-    def merge_branch(self, nlist1, nlist2, nodes):
-        if isinstance(nlist1, list):
+    def merge_branch(self, nlist1, nlist2, nlist3):
+        try:
+            self.connect_branch(nlist1)
             n1 = nlist1[-1]
-        else:
+        except:
             n1 = nlist1
-        if isinstance(nlist2, list):
+        try:
+            self.connect_branch(nlist2)
             n2 = nlist2[-1]
-        else:
+        except:
             n2 = nlist2
-        if isinstance(nodes, list):
-            self.connect_nodes_full(n1, nodes[0], 0)
-            self.connect_nodes_full(n2, nodes[0], 1)
-            self.connect_branch(nodes)
-        else:
-            self.connect_nodes_full(n1, nodes, 0)
-            self.connect_nodes_full(n2, nodes, 1)
+        try:
+            self.connect_branch(nlist3)
+            n3 = nlist3[0]
+        except:
+            n3 = nlist3
+ 
+        self.connect_nodes_full(n1, n3, 0)
+        self.connect_nodes_full(n2, n3, 1)
 
 class PluginManager(Ufo.PluginManager):
     def get_task(self, task, **kwargs):
