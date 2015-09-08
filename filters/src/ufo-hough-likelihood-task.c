fb84db55a0019fd5eadd840d0754b5c94c706e7c
Chuan Miao <chuan137@gmail.com>
2015-09-08 16:09:09 +0200

    hough-likelihood: correct misspelling

--- filters/src/ufo-hough-likelihood-task.c
+++ filters/src/ufo-hough-likelihood-task.c
@@ -23,10 +23,10 @@
 #include <CL/cl.h>
 #endif
 
-#include "ufo-hough-liklihood-task.h"
+#include "ufo-hough-likelihood-task.h"
 #include <glib/gprintf.h>
 
-struct _UfoHoughLiklihoodTaskPrivate {
+struct _UfoHoughLikelihoodTaskPrivate {
     gint numones;
     gint masksize;
     gint masksize_h;
@@ -39,11 +39,11 @@ struct _UfoHoughLiklihoodTaskPrivate {
 
 static void ufo_task_interface_init (UfoTaskIface *iface);
 
-G_DEFINE_TYPE_WITH_CODE (UfoHoughLiklihoodTask, ufo_hough_liklihood_task, UFO_TYPE_TASK_NODE,
+G_DEFINE_TYPE_WITH_CODE (UfoHoughLikelihoodTask, ufo_hough_likelihood_task, UFO_TYPE_TASK_NODE,
                          G_IMPLEMENT_INTERFACE (UFO_TYPE_TASK,
                                                 ufo_task_interface_init))
 
-#define UFO_HOUGH_LIKLIHOOD_TASK_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE((obj), UFO_TYPE_HOUGH_LIKLIHOOD_TASK, UfoHoughLiklihoodTaskPrivate))
+#define UFO_HOUGH_LIKELIHOOD_TASK_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE((obj), UFO_TYPE_HOUGH_LIKELIHOOD_TASK, UfoHoughLikelihoodTaskPrivate))
 
 enum {
     PROP_0,
@@ -55,23 +55,23 @@ enum {
 static GParamSpec *properties[N_PROPERTIES] = { NULL, };
 
 UfoNode *
-ufo_hough_liklihood_task_new (void)
+ufo_hough_likelihood_task_new (void)
 {
-    return UFO_NODE (g_object_new (UFO_TYPE_HOUGH_LIKLIHOOD_TASK, NULL));
+    return UFO_NODE (g_object_new (UFO_TYPE_HOUGH_LIKELIHOOD_TASK, NULL));
 }
 
 static void
-ufo_hough_liklihood_task_setup (UfoTask *task,
+ufo_hough_likelihood_task_setup (UfoTask *task,
                        UfoResources *resources,
                        GError **error)
 {
-    UfoHoughLiklihoodTaskPrivate *priv;
+    UfoHoughLikelihoodTaskPrivate *priv;
     cl_int err;
     int i, j, i0, j0;
     int *mask;
     float maskinnersize_h;
 
-    priv = UFO_HOUGH_LIKLIHOOD_TASK_GET_PRIVATE (task);
+    priv = UFO_HOUGH_LIKELIHOOD_TASK_GET_PRIVATE (task);
     priv->context = ufo_resources_get_context (resources);
     priv->kernel = ufo_resources_get_kernel (resources, "hough.cl", "likelihood", error);
 
@@ -105,7 +105,7 @@ ufo_hough_liklihood_task_setup (UfoTask *task,
 }
 
 static void
-ufo_hough_liklihood_task_get_requisition (UfoTask *task,
+ufo_hough_likelihood_task_get_requisition (UfoTask *task,
                                  UfoBuffer **inputs,
                                  UfoRequisition *requisition)
 {
@@ -113,38 +113,38 @@ ufo_hough_liklihood_task_get_requisition (UfoTask *task,
 }
 
 static guint
-ufo_hough_liklihood_task_get_num_inputs (UfoTask *task)
+ufo_hough_likelihood_task_get_num_inputs (UfoTask *task)
 {
     return 1;
 }
 
 static guint
-ufo_hough_liklihood_task_get_num_dimensions (UfoTask *task,
+ufo_hough_likelihood_task_get_num_dimensions (UfoTask *task,
                                              guint input)
 {
     return 2;
 }
 
 static UfoTaskMode
-ufo_hough_liklihood_task_get_mode (UfoTask *task)
+ufo_hough_likelihood_task_get_mode (UfoTask *task)
 {
     return UFO_TASK_MODE_PROCESSOR | UFO_TASK_MODE_GPU;
 }
 
 static gboolean
-ufo_hough_liklihood_task_process (UfoTask *task,
+ufo_hough_likelihood_task_process (UfoTask *task,
                          UfoBuffer **inputs,
                          UfoBuffer *output,
                          UfoRequisition *requisition)
 {
-    UfoHoughLiklihoodTaskPrivate *priv;
+    UfoHoughLikelihoodTaskPrivate *priv;
     UfoGpuNode *node;
     UfoProfiler *profiler;
     cl_command_queue cmd_queue;
     cl_mem in_image;
     cl_mem out_mem;
 
-    priv = UFO_HOUGH_LIKLIHOOD_TASK_GET_PRIVATE (task);
+    priv = UFO_HOUGH_LIKELIHOOD_TASK_GET_PRIVATE (task);
     node = UFO_GPU_NODE (ufo_task_node_get_proc_node (UFO_TASK_NODE (task)));
     profiler = ufo_task_node_get_profiler (UFO_TASK_NODE (task));
     cmd_queue = ufo_gpu_node_get_cmd_queue (node);
@@ -162,12 +162,12 @@ ufo_hough_liklihood_task_process (UfoTask *task,
 }
 
 static void
-ufo_hough_liklihood_task_set_property (GObject *object,
+ufo_hough_likelihood_task_set_property (GObject *object,
                               guint property_id,
                               const GValue *value,
                               GParamSpec *pspec)
 {
-    UfoHoughLiklihoodTaskPrivate *priv = UFO_HOUGH_LIKLIHOOD_TASK_GET_PRIVATE (object);
+    UfoHoughLikelihoodTaskPrivate *priv = UFO_HOUGH_LIKELIHOOD_TASK_GET_PRIVATE (object);
 
     switch (property_id) {
         case PROP_MASKSIZE:
@@ -189,12 +189,12 @@ ufo_hough_liklihood_task_set_property (GObject *object,
 }
 
 static void
-ufo_hough_liklihood_task_get_property (GObject *object,
+ufo_hough_likelihood_task_get_property (GObject *object,
                               guint property_id,
                               GValue *value,
                               GParamSpec *pspec)
 {
-    UfoHoughLiklihoodTaskPrivate *priv = UFO_HOUGH_LIKLIHOOD_TASK_GET_PRIVATE (object);
+    UfoHoughLikelihoodTaskPrivate *priv = UFO_HOUGH_LIKELIHOOD_TASK_GET_PRIVATE (object);
 
     switch (property_id) {
         case PROP_MASKSIZE:
@@ -210,30 +210,30 @@ ufo_hough_liklihood_task_get_property (GObject *object,
 }
 
 static void
-ufo_hough_liklihood_task_finalize (GObject *object)
+ufo_hough_likelihood_task_finalize (GObject *object)
 {
-    G_OBJECT_CLASS (ufo_hough_liklihood_task_parent_class)->finalize (object);
+    G_OBJECT_CLASS (ufo_hough_likelihood_task_parent_class)->finalize (object);
 }
 
 static void
 ufo_task_interface_init (UfoTaskIface *iface)
 {
-    iface->setup = ufo_hough_liklihood_task_setup;
-    iface->get_num_inputs = ufo_hough_liklihood_task_get_num_inputs;
-    iface->get_num_dimensions = ufo_hough_liklihood_task_get_num_dimensions;
-    iface->get_mode = ufo_hough_liklihood_task_get_mode;
-    iface->get_requisition = ufo_hough_liklihood_task_get_requisition;
-    iface->process = ufo_hough_liklihood_task_process;
+    iface->setup = ufo_hough_likelihood_task_setup;
+    iface->get_num_inputs = ufo_hough_likelihood_task_get_num_inputs;
+    iface->get_num_dimensions = ufo_hough_likelihood_task_get_num_dimensions;
+    iface->get_mode = ufo_hough_likelihood_task_get_mode;
+    iface->get_requisition = ufo_hough_likelihood_task_get_requisition;
+    iface->process = ufo_hough_likelihood_task_process;
 }
 
 static void
-ufo_hough_liklihood_task_class_init (UfoHoughLiklihoodTaskClass *klass)
+ufo_hough_likelihood_task_class_init (UfoHoughLikelihoodTaskClass *klass)
 {
     GObjectClass *oclass = G_OBJECT_CLASS (klass);
 
-    oclass->set_property = ufo_hough_liklihood_task_set_property;
-    oclass->get_property = ufo_hough_liklihood_task_get_property;
-    oclass->finalize = ufo_hough_liklihood_task_finalize;
+    oclass->set_property = ufo_hough_likelihood_task_set_property;
+    oclass->get_property = ufo_hough_likelihood_task_get_property;
+    oclass->finalize = ufo_hough_likelihood_task_finalize;
 
     properties[PROP_MASKSIZE] =
         g_param_spec_int ("masksize",
@@ -252,13 +252,13 @@ ufo_hough_liklihood_task_class_init (UfoHoughLiklihoodTaskClass *klass)
     for (guint i = PROP_0 + 1; i < N_PROPERTIES; i++)
         g_object_class_install_property (oclass, i, properties[i]);
 
-    g_type_class_add_private (oclass, sizeof(UfoHoughLiklihoodTaskPrivate));
+    g_type_class_add_private (oclass, sizeof(UfoHoughLikelihoodTaskPrivate));
 }
 
 static void
-ufo_hough_liklihood_task_init(UfoHoughLiklihoodTask *self)
+ufo_hough_likelihood_task_init(UfoHoughLikelihoodTask *self)
 {
-    self->priv = UFO_HOUGH_LIKLIHOOD_TASK_GET_PRIVATE(self);
+    self->priv = UFO_HOUGH_LIKELIHOOD_TASK_GET_PRIVATE(self);
     self->priv->masksize = 9;
     self->priv->maskinnersize = 2.0;
 }
