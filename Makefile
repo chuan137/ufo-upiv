ff96e0619405adf4a2c4f511bb4fd5f2516c6e7f
Chuan Miao <chuan137@gmail.com>
2015-10-01 16:50:57 +0200

    Add more test rules in Makefile

--- Makefile
+++ Makefile
@@ -2,29 +2,36 @@ SHELL := /bin/bash
 
 #SAMPLES := sampleB sampleC
 SAMPLES := sampleB
-FRAMES := 3
+FRAMES := 1
+
+frames := $$(seq 0 $$(($(FRAMES)-1)))
 
 TARGETDIR = data/res/
 CONFIGFILE = config_tmp.py
 INPUTPATH = data/
 OUTFILE = data/res_tmp.tif
 
-all : hough likelyhood
+all : hough likelyhood candidate
 
 .PHONY : tests all
 
 define run_single
 @for sample in $(SAMPLES); do \
+	if [ -n "$(3)" ]; then stdout="$(3)"; else stdout="/dev/null"; fi;\
+	echo $$stdout; \
 	cp config_$$sample.py $(CONFIGFILE); \
 	mkdir -p $(TARGETDIR)/$$sample; \
-	sed -ri "s/(graph = )[0-9]/\1\(2)/g" $(CONFIGFILE); \
+	sed -ri "s/(graph = )[0-9]/\1$(2)/g" $(CONFIGFILE); \
 	sed -ri "s~([^#]in_path\s+=\s+).*~\1'$(INPUTPATH)/$$sample',~g" $(CONFIGFILE); \
 	sed -ri "s~([^#]out_file\s+=\s+).*~\1'$(OUTFILE)',~g" $(CONFIGFILE); \
-	for key in `seq 1 $(FRAMES)`; do \
-		key=$$(( $$key - 1 )); \
+	for key in $(frames); do \
 		sed -ri "s/(\s+start\s+=\s+)[0-9]+/\1$$key/g" $(CONFIGFILE); \
-		python ./piv.py $(CONFIGFILE); \
-		cp $(OUTFILE) $(TARGETDIR)/$$sample/$(strip $(1))_$$key.tif; \
+		python ./piv.py $(CONFIGFILE) 1> $$stdout 2> /dev/null; \
+		if [ -n "$(3)" ]; then \
+			mv $$stdout $(TARGETDIR)/$$sample/$(strip $(3)).$$key; \
+		else \
+			mv $(OUTFILE) $(TARGETDIR)/$$sample/$(strip $(1))_$$key.tif; \
+		fi \
 		done \
 	done
 endef
@@ -33,7 +40,7 @@ define run_series
 @for sample in $(SAMPLES); do \
 	cp config_$$sample.py $(CONFIGFILE); \
 	mkdir -p $(TARGETDIR)/$$sample; \
-	sed -ri "s/(graph = )[0-9]/\1\(2)/g" $(CONFIGFILE); \
+	sed -ri "s/(graph = )[0-9]/\1$(2)/g" $(CONFIGFILE); \
 	sed -ri "s~([^#]in_path\s+=\s+).*~\1'$(INPUTPATH)/$$sample',~g" $(CONFIGFILE); \
 	sed -ri "s~([^#]out_file\s+=\s+).*~\1'$(OUTFILE)',~g" $(CONFIGFILE); \
 	sed -ri "s/([^#]\s+number\s+=\s+).*/\1$(FRAMES),/g" $(CONFIGFILE); \
@@ -45,15 +52,22 @@ endef
 
 
 tests :	
-	@echo $(sampleB_ROOT)
+	@echo $(frames)
+	@echo $(foreach ss, $(SAMPLES), config_$(ss).py)
 
-hough : config.py
-	@echo $@
+hough : $(foreach ss, $(SAMPLES), config_$(ss).py)
+	@echo -e "\nBuilding $@ ..."
 	$(call run_single, $@, 3)
 
-likelyhood : config.py
-	@echo $@
-	@cp $< $(CONFIGFILE)
-	@sed -ri "s/(graph = )[0-9]/\12/g" $(CONFIGFILE)
-	$(call run_single, $@)
-	@rm $(CONFIGFILE)*
+likelyhood : $(foreach ss, $(SAMPLES), config_$(ss).py)
+	@echo -e "\nBuilding $@ ..."
+	$(call run_single, $@, 2)
+
+candidate: $(foreach ss, $(SAMPLES), config_$(ss).py)
+	@echo -e "\nBuilding $@ ..."
+	$(call run_single, $@, 1, cand.txt)
+
+.PHONY : clean
+
+clean:
+	rm -rf $(foreach ss, $(SAMPLES), $(TARGETDIR)/$(ss))
---
+++ piv-test.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# useage:
+#   ./piv-hough.py [config filename]
+# default config file is config.py
+import sys
+import os
+from python.pivjob import PivJob
+import logging
+
+logfile = '.hough.log'
+logging.basicConfig(level=logging.DEBUG, filename=logfile, filemode='w', 
+                    format='%(name)s %(levelname)s %(message)s')
+
+try:
+    cf = __import__(os.path.splitext(sys.argv[1])[0])
+    parms = cf.parms
+    config = cf.config
+except:
+    from config import parms, config
+
+class PivJob(PivJob):
+    def setup_tasks(self):
+        self.setup_basic_tasks()
+        p = self.parms
+        sc = self.parms.scale
+
+        self.add_task('crop', x=p.xshift, y=p.yshift, width=p.width, height=p.height)
+        self.add_task('contrast', 'piv_contrast', 
+                c1=p.contrast_c1, c2=p.contrast_c2,
+                c3=p.contrast_c3, c4=p.contrast_c4)
+        self.add_task('rescale', factor=1.0/sc)
+        self.add_task('input_fft', 'fft', dimensions=2)
+        self.add_copy_task('bc_image') 
+
+        self.add_task('ring_fft', 'fft', dimensions=2)
+        self.add_task('ring_stack', 'stack', number=p.ring_number)
+        self.add_task('ring_loop', 'loop', count=p.number)
+        self.add_task('ring_convolution', 'complex_mult')
+        self.add_task('ring_slice', 'slice')
+        self.add_task('ring_pattern', 
+                start=p.ring_start/sc, end=p.ring_end/sc, step=p.ring_step/sc, 
+                thickness=p.ring_thickness/sc, method=p.ring_method, 
+                width=p.width/sc, height=p.height/sc)
+
+        self.add_task('ifft', dimensions=2)
+        self.add_task('likelihood', 'hough-likelihood', 
+                masksize=p.likelihoodmask, maskinnersize=p.likelihoodmaskinner)
+        self.add_task('cand', 'candidate-filter', 
+                threshold=p.candi_threshold, ring_start=p.ring_start, 
+                ring_step=p.ring_step, ring_end=p.ring_end )
+        self.add_task('azimu', 'azimuthal-test')
+
+    def setup_graph(self, flag):
+        if flag==0:
+            b1 = self.branch('read', 'crop', 'bc_image', 'rescale', 'contrast', 'input_fft')
+            b2 = self.branch('ring_pattern', 'ring_stack', 'ring_fft', 'ring_loop')
+            b3 = self.branch('bc_image')
+            b4 = self.branch('ring_convolution', 'ifft', 'likelihood', 'cand')
+            b5 = self.branch('azimu', 'ring_writer')
+            self.graph.merge_branch(b1, b2, b4)
+            self.graph.merge_branch(b3, b4, b5)
+        elif flag==1: # candidate
+            b1 = self.branch('read', 'crop', 'rescale', 'contrast', 'input_fft')
+            b2 = self.branch('ring_pattern', 'ring_stack', 'ring_fft', 'ring_loop')
+            b3 = self.branch('ring_convolution', 'ifft', 'likelihood', 'cand', 'ring_writer')
+            self.graph.merge_branch(b1, b2, b3)
+        elif flag==2: # likelihood
+            b1 = self.branch('read', 'crop', 'rescale', 'contrast', 'input_fft')
+            b2 = self.branch('ring_pattern', 'ring_stack', 'ring_fft', 'ring_loop')
+            b3 = self.branch('ring_convolution', 'ifft', 'likelihood', 'write')
+            self.graph.merge_branch(b1, b2, b3)
+        elif flag==3: # hough
+            b1 = self.branch('read', 'crop', 'rescale', 'contrast', 'input_fft')
+            b2 = self.branch('ring_pattern', 'ring_stack', 'ring_fft', 'ring_loop')
+            b3 = self.branch('ring_convolution', 'ifft', 'write')
+            self.graph.merge_branch(b1, b2, b3)
+        elif flag==4:
+            b1 = self.branch('read', 'crop', 'rescale', 'contrast', 'write')
+        else:
+            pass
+
+j = PivJob(parms)
+j.profiling = config.get('profiling') or False
+j.deviceCPU = config.get('deviceCPU') or False
+j.schedfixed = config.get('schedfixed') or True
+j.run(config.get('graph'))
