56ae9081664ee39dd081bb96e3a30028bbbccef8
Chuan Miao <chuan137@gmail.com>
2015-10-16 13:00:28 +0200

    ring-writer: add message: detected rings number

--- filters/src/ufo-ring-writer-task.c
+++ filters/src/ufo-ring-writer-task.c
@@ -98,6 +98,7 @@ ufo_ring_writer_task_process (UfoTask *task,
     unsigned num = in_mem[0]; 
     unsigned scale = in_mem[1];
 
+    g_message("RingWriter: detected ring number %4u", num);
     fprintf(priv->fp, "# RingWriter: number of rings %u\n", num);
     for (unsigned  i = 0; i < num; i++) {
         fprintf(priv->fp, 
