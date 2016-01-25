4048702ae97a7a0aa818440c0f805a0ef8991ee7
Chuan Miao <chuan137@gmail.com>
2016-01-25 12:39:33 +0100

    improve dir structure

--- SourceMe.sh
+++ SourceMe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-export LOCAL_PLUGIN_DIR=$DIR/.build
-export LOCAL_KERNEL_DIR=$DIR/kernels
+export LOCAL_PLUGIN_DIR=$DIR/ufo/.build
+export LOCAL_KERNEL_DIR=$DIR/ufo/kernels
 export UFO_PLUGIN_PATH=$LOCAL_PLUGIN_DIR:$UFO_PLUGIN_PATH
 export UFO_KERNEL_PATH=$LOCAL_KERNEL_DIR:$UFO_KERNEL_PATH
